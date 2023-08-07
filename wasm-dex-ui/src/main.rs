d1c5ee510ecd6285271a0613c3dc20e2c2af1ba0
Tomasz Waszczyk <tomasz@waszczyk.com>
2023-08-08 00:13:45 +0200

    add(lib): debug dependencies

--- wasm-dex-ui/src/main.rs
+++ wasm-dex-ui/src/main.rs
@@ -1,8 +1,13 @@
 #![allow(non_snake_case)]
 // get access to the `rsx!` macro and the `Scope` and `Element` types
 use dioxus::prelude::*;
+use log::LevelFilter;
 
 fn main() {
+    dioxus_logger::init(LevelFilter::Info).expect("Failed to initialize logger");
+    console_error_panic_hook::set_once();
+
+    log::info!("Starting WebAssembly Decentralized Exchange");
     dioxus_web::launch(App);
 }
 
